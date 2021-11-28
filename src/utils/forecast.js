7164b685b9d743934eac41af2f091bfc123dd68a
BANDHAN147 <raihansultan05@gmail.com>
2021-11-29 01:47:08 +0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+const forecast= (latitude, longitude, callback)=>{
+
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,daily&appid=b6a831fcdc26a4464d21f9f1589dfa55'
+    request({url, json:true}, (error, {body})=>{
+        if (error){
+            callback('Unable to connect weather forecast service', undefined)
+    
+        }else if(body.message){
+            callback(body.message, undefined)
+        } 
+    
+        else{
+            callback(undefined, {
+                weather: body.current.weather[0].main,
+                description: body.current.weather[0].description,
+            })
+    
+        }
+    }) 
+}
+
+module.exports = forecast
